e4131c9b144bbbf5a2bc93667eac2dc2e45cbeb3
PrinMax <max.prin@edu.univ-fcomte.fr>
2024-04-12 16:24:22 +0200

    ajout bouton et li

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,17 @@ onErrorCaptured((err, instance, info) => {
 
 <template>
   <header>
+    <button aria-controls="mainNav" aria-expanded="true" class="rounded-full border-2 border-red-600 bg-red-300 px-2">
+      menu
+    </button>
+    <!-- nav#mainNav>ul>li*3>a[href="#"]{item $} -->
+    <nav id="mainNav">
+    </nav>
     <nav>
       <ul>
-        <li>
-          <RouterLink to="/" class="text-red-500 underline"> Accueil </RouterLink>
-        </li>
+        <li><a href="#">item 1</a></li>
+        <li><a href="#">item 2</a></li>
+        <li><a href="#">item 3</a></li>
       </ul>
     </nav>
   </header>
@@ -26,14 +32,4 @@ onErrorCaptured((err, instance, info) => {
   </RouterView>
 </template>
 
-<button aria-controls="mainNav" aria-expanded="true" class="rounded-full border-2 border-red-600 bg-red-300 px-2">
-  menu
-</button>
-<!-- nav#mainNav>ul>li*3>a[href="#"]{item $} -->
-<nav id="mainNav">
-  <ul>
-    <li><a href="#">item 1</a></li>
-    <li><a href="#">item 2</a></li>
-    <li><a href="#">item 3</a></li>
-  </ul>
-</nav>
+
